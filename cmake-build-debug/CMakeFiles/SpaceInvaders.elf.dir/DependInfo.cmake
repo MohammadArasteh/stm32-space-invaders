e3d4ec5f766a2dc7341380e8115030d40a003cef
Mohammad Arasteh <arasteh@gmail.com>
2023-02-07 02:22:27 +0330

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SpaceInvaders.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Startup/startup_stm32f303vctx.s" "C:/Users/alireza/CLionProjects/SpaceInvaders/cmake-build-debug/CMakeFiles/SpaceInvaders.elf.dir/Core/Startup/startup_stm32f303vctx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F303xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Inc"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Inc"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/LiquidCrystal.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/LiquidCrystal.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/LiquidCrystal.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/main.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/stm32f3xx_hal_msp.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/stm32f3xx_hal_msp.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/stm32f3xx_hal_msp.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/stm32f3xx_it.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/stm32f3xx_it.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/stm32f3xx_it.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/syscalls.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/sysmem.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Core/Src/system_stm32f3xx.c" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/system_stm32f3xx.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/system_stm32f3xx.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj.d"
+  "C:/Users/alireza/CLionProjects/SpaceInvaders/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj" "gcc" "CMakeFiles/SpaceInvaders.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
