de4042dc309512da7dde7eb88fc6a054043c0062
SMhd <SMhd3000@gmail.com>
2023-02-08 00:37:28 +0330

    changes after adding cmake-build-debug to gitignore

--- cmake-build-debug/CMakeFiles/SpaceInvaders.elf.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/SpaceInvaders.elf.dir/cmake_clean.cmake
@@ -11,8 +11,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/LCD.c.obj.d"
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/LiquidCrystal.c.obj"
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/LiquidCrystal.c.obj.d"
-  "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/MenuScreen.c.obj"
-  "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/MenuScreen.c.obj.d"
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/UART.c.obj"
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/UART.c.obj.d"
   "CMakeFiles/SpaceInvaders.elf.dir/Core/Src/Utils.c.obj"
